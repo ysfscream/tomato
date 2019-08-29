16eebfcaceb472989b4d0eba6ba3f91efa308bf6
ysfscream <894402575@qq.com>
2019-08-30 00:20:36 +0800

    stash codes

--- app/main.dev.js
+++ app/main.dev.js
@@ -10,8 +10,9 @@
  *
  * @flow
  */
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, Tray } from 'electron';
 import { autoUpdater } from 'electron-updater';
+import * as path from 'path'
 import log from 'electron-log';
 import MenuBuilder from './menu';
 
@@ -26,6 +27,55 @@ export default class AppUpdater {
 }
 
 let mainWindow = null;
+let tray = null;
+
+const createMainWindow = () => {
+  mainWindow = new BrowserWindow({
+    // 顶部系统的操作按钮栏（全屏，缩小，关闭）
+    frame: false,
+    // 窗口是否可以改变尺寸
+    resizable: true,
+    // 使窗口透明. 默认值为 false
+    // transparent: true,
+    show: false,
+    width: 240,
+    height: 400,
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/app.html`);
+
+  // @TODO: Use 'ready-to-show' event
+  //        https://github.com/electron/electron/blob/master/docs/api/browser-window.md#using-ready-to-show-event
+  mainWindow.webContents.on('did-finish-load', () => {
+    if (!mainWindow) {
+      throw new Error('"mainWindow" is not defined');
+    }
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+
+  const menuBuilder = new MenuBuilder(mainWindow);
+  menuBuilder.buildMenu();
+};
+
+const createTray = () => {
+  const iconPath = path.join(__dirname, '../resources', 'tomato.png');
+  tray = new Tray(iconPath);
+  tray.on('click', () => {
+    showWindow()
+  });
+};
+
+const showWindow = () => {
+  if (process.env.START_MINIMIZED && mainWindow) {
+    mainWindow.minimize();
+  } else {
+    mainWindow.show();
+    mainWindow.focus();
+  }
+};
 
 if (process.env.NODE_ENV === 'production') {
   const sourceMapSupport = require('source-map-support');
@@ -69,34 +119,8 @@ app.on('ready', async () => {
     await installExtensions();
   }
 
-  mainWindow = new BrowserWindow({
-    show: false,
-    width: 1024,
-    height: 728
-  });
-
-  mainWindow.loadURL(`file://${__dirname}/app.html`);
-
-  // @TODO: Use 'ready-to-show' event
-  //        https://github.com/electron/electron/blob/master/docs/api/browser-window.md#using-ready-to-show-event
-  mainWindow.webContents.on('did-finish-load', () => {
-    if (!mainWindow) {
-      throw new Error('"mainWindow" is not defined');
-    }
-    if (process.env.START_MINIMIZED) {
-      mainWindow.minimize();
-    } else {
-      mainWindow.show();
-      mainWindow.focus();
-    }
-  });
-
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
-
-  const menuBuilder = new MenuBuilder(mainWindow);
-  menuBuilder.buildMenu();
+  createMainWindow();
+  createTray();
 
   // Remove this if your app does not use auto updates
   // eslint-disable-next-line
